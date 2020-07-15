b5843856c1c0b25340b543f363826866d888480c
kannychak <kannychak@qq.com>
2020-07-15 16:01:33 +0800

    add syncthing

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,11 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+sed -i '$a src-link custom /custom' feeds.conf.default
 
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/garypang13/openwrt-adguardhome package/openwrt-adguardhome
 #git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
 
-git clone https://github.com/kannychak/luci-app-syncthing feeds/luci/applications/luci-app-syncthing
+git clone https://github.com/kannychak/luci-app-syncthing /custom/luci-app-syncthing

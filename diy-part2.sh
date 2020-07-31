698e41b91810b723f931f69199770c3909b53acd
Kanny.Chak <kannychak@qq.com>
2020-07-31 17:07:57 +0800

    Update diy-part2.sh
    删除更新后原来的应用、
    rm -rf ./package/lean/luci-theme-argon
    rm -rf ./feeds/packages/net/smartdns
    rm -rf ./feeds/packages/utils/syncthing

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/utils/syncthing
+

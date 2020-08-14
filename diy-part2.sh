c20c6924d72dc766f0520f2aae5d06fab2ac88a1
kannychak <kannychak@qq.com>
2020-08-14 14:35:01 +0800

    修改更新插件的版本号的方式

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,11 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 rm -rf ./package/lean/luci-theme-argon
-rm -rf ./feeds/packages/net/smartdns
+# rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/luci/luci-app-syncthing
-rm -rf ./feeds/packages/utils/syncthing
-rm -rf ./feeds/packages/utils/adguardhome
+# rm -rf ./feeds/packages/utils/syncthing
+# rm -rf ./feeds/packages/utils/adguardhome
+
+# 更新部份插件的版本
+sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=5.2020.04/g};/PKG_SOURCE_VERSION:=/{s/.*/PKG_SOURCE_VERSION:=Release31/g}' ./feeds/packages/net/smartdns/Makefile
+sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=v1.7.1/g}' ./feeds/packages/utils/syncthing/Makefile

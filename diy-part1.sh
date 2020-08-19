d0fa9ad2d4fa5779f945d67dfdc08f14635acd6d
kannychak <kannychak@qq.com>
2020-08-19 18:03:41 +0800

    更新编译流程

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 # sed -i '$a src-link custom /custom' feeds.conf.default
 # sed -i '$a src-git myfeeds https://github.com/kannychak/openwrt_feeds' feeds.conf.default
-
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-# git clone https://github.com/garypang13/openwrt-adguardhome package/openwrt-adguardhome
-git clone https://github.com/pymumu/luci-app-smartdns -b lede package/luci-app-smartdns
-# git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06 package/luci-theme-argon
-git clone https://github.com/kannychak/luci-app-syncthing package/luci-app-syncthing
-# svn co https://github.com/kannychak/openwrt_feeds/trunk/syncthing package/syncthing
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,14 +12,16 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 rm -rf ./package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon -b 18.06 ./package/lean/luci-theme-argon
+
 # rm -rf ./feeds/packages/net/smartdns
-rm -rf ./feeds/luci/luci-app-syncthing
+# rm -rf ./feeds/luci/luci-app-syncthing
 # rm -rf ./feeds/packages/utils/syncthing
 # rm -rf ./feeds/packages/utils/adguardhome
 
 # 更新部份插件的版本
 sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=5.2020.04/g};/PKG_SOURCE_VERSION:=/{s/.*/PKG_SOURCE_VERSION:=Release31/g}' ./feeds/packages/net/smartdns/Makefile
-sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=v1.7.1/g}' ./feeds/packages/utils/syncthing/Makefile
+sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=v1.8.0/g}' ./feeds/packages/utils/syncthing/Makefile
 
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by Kenny'/g" package/base-files/files/etc/openwrt_release

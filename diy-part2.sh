b20ab5370b9ac329a27f2e6648edac4f1786d1a2
Kanny.Chak <kannychak@qq.com>
2020-08-24 10:39:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,7 +29,7 @@ sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=5.2020.04/g};/PKG_SOURCE_VERSION:=/{s/
 # rm -rf ./feeds/packages/utils/syncthing
 # svn co https://github.com/kannychak/openwrt_feeds/trunk/syncthing ./feeds/packages/utils/syncthing
 sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=1.8.0/g}' ./feeds/packages/utils/syncthing/Makefile
-sed -i '/PKG_HASH:=/{s/.*/#PKG_HASH:=0abe7891d689edf5e148d257e4264e9764e4479cb935f53ccc2e3c07efc87bf0/g}' ./feeds/packages/utils/syncthing/Makefile
+#sed -i '/PKG_HASH:=/{s/.*/#PKG_HASH:=0abe7891d689edf5e148d257e4264e9764e4479cb935f53ccc2e3c07efc87bf0/g}' ./feeds/packages/utils/syncthing/Makefile
 
 date=`date +%m.%d.%Y`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by Kenny'/g" package/base-files/files/etc/openwrt_release

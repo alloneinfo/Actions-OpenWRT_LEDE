113e4cd34a20a77f34cd3f46d7c93c3c6663ef91
kannychak <kannychak@qq.com>
2020-09-10 13:17:13 +0800

    更新版本方式

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,8 +37,8 @@ sed -i "s/option mediaurlbase.*/option mediaurlbase '\/luci-static\/argon'/g" ./
 rm -rf ./feeds/packages/net/smartdns
 git clone https://github.com/alloneinfo/openwrt-smartdns ./feeds/packages/net/smartdns
 #sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=8.2020.24/g};/PKG_SOURCE_VERSION:=/{s/.*/PKG_SOURCE_VERSION:=Release32/g}' ./feeds/packages/net/smartdns/Makefile
-sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=8.2020.27/g}' ./feeds/packages/net/smartdns/Makefile
-sed -i '/PKG_SOURCE_VERSION:=/{s/.*/PKG_SOURCE_VERSION:=7af6f475da372842c48f570f44b52f48616b11d6/g}' ./feeds/packages/net/smartdns/Makefile
+sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=9.2020.8/g}' ./feeds/packages/net/smartdns/Makefile
+sed -i '/PKG_SOURCE_VERSION:=/{s/.*/PKG_SOURCE_VERSION:=86902d2e3415666246dae0286262e43923f58532/g}' ./feeds/packages/net/smartdns/Makefile
 
 # 替换 luci-app-zerotier
 rm -rf ./package/lean/luci-app-zerotier
@@ -47,6 +47,17 @@ git clone https://github.com/alloneinfo/luci-app-zerotier ./package/lean/luci-ap
 # 替换 Syncthing 并更新版本
 # rm -rf ./feeds/packages/utils/syncthing
 # svn co https://github.com/alloneinfo/openwrt_feeds/trunk/syncthing ./feeds/packages/utils/syncthing
-# sed -i '/PKG_HASH:=/{s/.*/PKG_HASH:=skip/g}' ./feeds/packages/utils/syncthing/Makefile
-sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=1.8.0/g}' ./feeds/packages/utils/syncthing/Makefile
-sed -i '/PKG_HASH:=/{s/.*/PKG_HASH:=04d78fbe6015334c284bf46ffcf8feb6b2b690ef810f2f0c5732cdee5cd8142a/g}' ./feeds/packages/utils/syncthing/Makefile
+sed -i '/PKG_HASH:=/{s/.*/PKG_HASH:=skip/g}' ./feeds/packages/utils/syncthing/Makefile
+# sed -i '/PKG_VERSION:=/{s/.*/PKG_VERSION:=1.8.0/g}' ./feeds/packages/utils/syncthing/Makefile
+# sed -i '/PKG_HASH:=/{s/.*/PKG_HASH:=04d78fbe6015334c284bf46ffcf8feb6b2b690ef810f2f0c5732cdee5cd8142a/g}' ./feeds/packages/utils/syncthing/Makefile
+
+getversion(){
+ver=$(basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$1/releases/latest) | grep -o -E "[0-9].+")
+[ $ver ] && echo $ver || git ls-remote --tags git://github.com/$1 | cut -d/ -f3- | sort -t. -nk1,2 -k3 | awk '/^[^{]*$/{version=$1}END{print version}' | grep -o -E "[0-9].+"
+}
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion v2ray/v2ray-core)/g" package/*/v2ray/Makefile
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion syncthing/syncthing)/g" ./feeds/packages/utils/syncthing/Makefile
+# sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion pymumu/smartdns)/g" ./feeds/packages/net/smartdns/Makefile
+sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$(getversion AdguardTeam/AdGuardHome)/g" package/feeds/custom/adguardhome/Makefile
+
+sed -i "s/PKG_HASH:=.*/PKG_HASH:=skip/g" package/feeds/custom/*/Makefile

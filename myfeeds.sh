97e63def225ad8a15372f12398b2c0fc074387a3
kannychak <kannychak@qq.com>
2020-09-28 14:26:32 +0800

    更换 hc5861.config

--- myfeeds.sh
+++ myfeeds.sh
@@ -3,6 +3,8 @@ git clone https://github.com/alloneinfo/openwrt-adguardhome ./myfeeds/adguardhom
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ./myfeeds/luci-app-adguardhome
 git clone https://github.com/kongfl888/luci-app-adguardhome ./myfeeds/luci-app-adguardhome
 
+git clone https://github.com/rufengsuixing/luci-app-autoipsetadder ./myfeeds/luci-app-autoipsetadder
+
 git clone https://github.com/pymumu/luci-app-smartdns -b lede ./myfeeds/luci-app-smartdns
 
 git clone https://github.com/alloneinfo/luci-app-syncthing ./myfeeds/luci-app-syncthing

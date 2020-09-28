7709c9ac0071afbb19f4bfdd5e3e0df5eb1b1848
kannychak <kannychak@qq.com>
2020-09-28 12:01:25 +0800

    更换 luci-app-adguardhome

--- myfeeds.sh
+++ myfeeds.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/alloneinfo/openwrt-adguardhome ./myfeeds/adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome ./myfeeds/luci-app-adguardhome
+
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome ./myfeeds/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome ./myfeeds/luci-app-adguardhome
 
 git clone https://github.com/pymumu/luci-app-smartdns -b lede ./myfeeds/luci-app-smartdns
 
